9e03c03d81ad53df1341982919c220509a7f93b8
anhpham21u <hoctap31111@gmail.com>
2025-02-20 15:18:58 +0700

    hot fix

--- book/urls.py
+++ book/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import book_list, book_create, book_update
 
 urlpatterns = [
-    # path('book-list/', book_list, name='book_list'),
+    path('list/', book_list, name='book_list'),
     path('new/', book_create, name='book_create'),
     path('<int:pk>/edit/', book_update, name='book_update'),
 ]
